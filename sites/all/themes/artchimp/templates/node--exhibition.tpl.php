dff33ede8617d1f8d924f04de617daa32a5e9884
Light Server <light@dev.lightrains.com>
2017-09-14 18:17:58 +0530

    Added design for exhibition details page

---
+++ sites/all/themes/artchimp/templates/node--exhibition.tpl.php
@@ -0,0 +1,63 @@
+        <div class="container p-140-cont">
+          <div class="row">
+
+            <!-- CONTENT -->
+            <div class="col-sm-8 blog-main-posts">
+
+              <!-- POST ITEM -->
+              <div class="blog-post wow fadeIn pb-50">
+                <div class="post-prev-img">
+                  <a href="blog-single-sidebar-right.html"><img src="images/blog/post-wide-1.jpg" alt="img"></a>
+                </div>
+
+                <div class="post-prev-title">
+                  <h3><a href="blog-single-sidebar-right.html">TIME FOR MINIMALISM</a></h3>
+                </div>
+
+                <div class="post-prev-info">
+                  JULE 10<span class="slash-divider">/</span><a href="http://themeforest.net/user/abcgomel/portfolio?ref=abcgomel">JOHN DOE</a><span class="slash-divider">/</span><a href="#">DESIGN</a>, <a href="#">DEVELOPMENT</a>
+                </div>
+
+                <div class="post-prev-text">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, nostrum, cumque culpa provident aliquam commodi assumenda laudantium magnam illo nostrum. Donec nibh sapien, molestie quis elementum et, dim non atino ipsum.</p>
+                  <p>Fusce non ante sed lorem rutrum feugiat. Vestibulum pellentesque, purus ut&nbsp;dignissim consectetur, nulla erat ultrices purus, ut&nbsp;consequat sem elit non sem.
+                  Morbi lacus massa, euismod ut turpis molestie, tristique sodales est. Integer sit amet mi id sapien tempor molestie in nec massa.
+                  Fusce non ante sed lorem rutrum feugiat.</p>
+
+                  <blockquote class="mb-40 mt-40">
+                    <p>Vivamus sit amet facilisis metus. Fusce felis libero, hendrerit eu plac erat at, facilisis sit amet libero. Etiam eget interdum lorem. </p>
+                    <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                  </blockquote>
+
+                  <p>Pellentesque venenatis tellus non purus tincidunt vitae ultrices tellus eleifend. Praesent quam augue, accumsan nec tempus dapibus, pharetra ac lacus. Nunc eleifend consequat justo id dapibus. In ut consequat massa. Nunc scelerisque suscipit leo nec imperdiet. Vestibulum pulvinar adipiscing turpis vitae ultrices. Suspendisse eu lectus dui, vitae lobortis lorem. Fusce gravida nibh et ante accusan molestie.</p>
+
+
+                  <p>Fusce id dui sem. Cras gravida odio et magna faucbus iaculis. Suspendisse eu lectus dui, vitae lobortis lorem. Fusce gravida nibh et ante accusan molestie. Duis convallis semper felis. Curabitur fringilla placerat vestibulum. Aenean dignissim libero et quam tristique vel vehicula nunc suscipit. Fusce id dui sem.  Cras gravida odio et magna faucbus iaculis. Vestibulum ante ipsum primis in faucibus orci. Curabitur fringilla placerat vestibulum. Aenean dignissim libero et quam tristique nunc suscipit.</p>
+
+                </div>
+
+              </div>
+
+              <!-- <div class="post-author-container clearfix mb-50">
+
+                <div class="post-author-avatar">
+                  <img alt="ava" src="images/content/avatar-1.png" >
+                </div>
+
+                <div class="comment-content">
+
+                  <div class="comment-author">
+                    <a href="http://themeforest.net/user/abcgomel/portfolio?ref=abcgomel">JOHN DOE</a>
+                  </div>
+
+                  <p>CEO & Founder Haswell. Data lover, hobby chef, econometrician with strong experiences in the finance industry. </p>
+
+                </div>
+              </div> -->
+
+           <!--    <hr class="mt-0 mb-0">
+              <hr class="mt-30 mb-0"> -->
+
+            </div>
+          </div>
+        </div>
