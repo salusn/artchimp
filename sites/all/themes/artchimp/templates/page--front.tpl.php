f9d8e7e6da9c6bf2b8ee2a0d7b836d742e66e2d5
Jikku Jose <jikkujose@gmail.com>
2017-10-09 23:48:46 +0530

    Remove newsletter

--- sites/all/themes/artchimp/templates/page--front.tpl.php
+++ sites/all/themes/artchimp/templates/page--front.tpl.php
@@ -113,25 +113,6 @@ $path_to_theme = $base_url . '/' . drupal_get_path('theme', 'artchimp');?>
           </div>
         </div>
 
-        <!-- NEWS LETTER -->
-        <div class="page-section nl-cont">
-          <div class="container">
-            <div class="relative" >
-              <div id="mc_embed_signup" class="nl-form-container clearfix">
-                <form action="http://abcgomel.us9.list-manage.com/subscribe/post-json?u=ba37086d08bdc9f56f3592af0&amp;id=e38247f7cc&amp;c=?" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="newsletterform validate" target="_blank" novalidate>   <!-- EDIT THIS ACTION URL (add "post-json?u" instead of "post?u" and appended "&amp;c=?" to the end of this URL) -->
-                  <input type="email" value="" name="EMAIL" class="email nl-email-input" id="mce-EMAIL" placeholder="Your@email.com" required>
-                  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                  <div style="position: absolute; left: -5000px;"><input type="text" name="b_ba37086d08bdc9f56f3592af0_e38247f7cc" tabindex="-1" value=""></div>
-
-                  <input type="submit" value="SUBSCRIBE" name="subscribe" id="mc-embedded-subscribe" class="button medium gray">
-                </form>
-                <div id="notification_container"  ></div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
         <!-- FOOTER 2 BLACK -->
         <footer class="page-section pb-20 footer2-black">
           <div class="container">
