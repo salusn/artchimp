2d7d2779ceeed37aac04c86d5496bf3ed6f9a99d
Jikku Jose <jikkujose@gmail.com>
2017-10-10 00:18:55 +0530

    Formatting..

--- sites/all/themes/artchimp/templates/page.tpl.php
+++ sites/all/themes/artchimp/templates/page.tpl.php
@@ -86,11 +86,11 @@
 
           <div class="col-md-4">
 
-            <?php if ($breadcrumb): ?>
+            <?php if ($breadcrumb) : ?>
             <div class="breadcrumbs">
-              <?php print $breadcrumb;?>
+                <?php print $breadcrumb;?>
             </div>
-            <?php endif;?>
+            <?php endif; ?>
 
           </div>
 
@@ -98,61 +98,37 @@
       </div>
     </div>
 
-    <?php if ($messages): ?>
-    <div class="page-section pt-80">
-      <div class="container">
-        <?php print $messages;?>
+    <?php if ($messages) : ?>
+      <div class="page-section pt-80">
+        <div class="container">
+            <?php print $messages;?>
+        </div>
       </div>
-    </div>
     <?php endif;?>
 
     <div class="page-section p-80-cont">
       <div class="container">
         <div class="row">
-
-          <?php if ($page[ 'sidebar_first']): ?>
-          <div id="sidebar-second" class="col-sm-4 col-md-3">
-            <?php print render($page[ 'sidebar_first']);?>
-          </div>
-          <div id="main-content" class="col-sm-8">
-            <?php print render($page[ 'content']);?>
-          </div>
-          <?php else: ?>
-          <?php print render($page[ 'content']);?>
-          <?php endif;?>
-
-        </div>
-      </div>
-    </div>
-
-    <?php if ($page[ 'content_footer']): ?>fdfddfd
-    <hr class="mt-0 mb-80">
-    <div class="container">
-      <?php print render($page[ 'content_footer']);?>
-    </div>
-    <?php endif;?>
-
-
-    <!-- NEWS LETTER -->
-    <div class="page-section nl-cont">
-      <div class="container">
-        <div class="relative">
-          <div id="mc_embed_signup" class="nl-form-container clearfix">
-            <form action="http://abcgomel.us9.list-manage.com/subscribe/post-json?u=ba37086d08bdc9f56f3592af0&amp;id=e38247f7cc&amp;c=?" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="newsletterform validate" target="_blank" novalidate>
-              <!-- EDIT THIS ACTION URL (add "post-json?u" instead of "post?u" and appended "&amp;c=?" to the end of this URL) -->
-              <input type="email" value="" name="EMAIL" class="email nl-email-input" id="mce-EMAIL" placeholder="Enter your email" required>
-              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-              <div style="position: absolute; left: -5000px;">
-                <input type="text" name="b_ba37086d08bdc9f56f3592af0_e38247f7cc" tabindex="-1" value="">
+            <?php if ($page['sidebar_first']) : ?>
+              <div id="sidebar-second" class="col-sm-4 col-md-3">
+                <?php print render($page[ 'sidebar_first']);?>
               </div>
-
-              <input type="submit" value="SUBSCRIBE" name="subscribe" id="mc-embedded-subscribe" class="button medium gray">
-            </form>
-            <div id="notification_container"></div>
-          </div>
+              <div id="main-content" class="col-sm-8">
+                <?php print render($page[ 'content']);?>
+              </div>
+            <?php else : ?>
+            <?php print render($page[ 'content']); ?>
+            <?php endif; ?>
         </div>
       </div>
     </div>
+
+    <?php if ($page[ 'content_footer']) : ?>
+      <hr class="mt-0 mb-80">
+      <div class="container">
+        <?php print render($page[ 'content_footer']);?>
+      </div>
+    <?php endif;?>
 
     <!-- FOOTER 2 BLACK -->
     <footer class="page-section pb-20 footer2-black">
