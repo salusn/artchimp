10b385f2988403e5c21628cc55fe156bfe5348d9
Light Server <light@dev.lightrains.com>
2017-10-10 11:49:13 +0530

    Artist name changes

--- sites/all/themes/artchimp/views/views-view--list-artists--page-1.tpl.php
+++ sites/all/themes/artchimp/views/views-view--list-artists--page-1.tpl.php
@@ -1,17 +1,18 @@
-<?php if ($rows) : ?>
+<?php if ($rows): ?>
 <?php foreach ($view
-    ->style_plugin->rendered_fields as $key => $value) {
-?>
+	->style_plugin->rendered_fields as $key => $value) {
+	?>
 <div class="member col-md-4 col-sm-3">
     <div class="member-image">
         <?php echo $value['field_photo'] ?>
     </div>
     <?php
-    $firstname = $value['title'] . ' ' . $value['field_lastname'];
-?>
-    <div class="f2 b mt4"><?php echo $firstname; ?></div>
+$firstname = $value['title'] . ' ' . $value['field_lastname'];
+	?>
+
+    <div class="f2 b mt4"><?php echo $value['nothing']; ?></div>
 </div>
 <?php
-} ?>
+}?>
 <?php
-endif; ?>
+endif;?>
