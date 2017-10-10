2b184cb5a2a7dadb4bfb8d800e5494b4e3dabc59
Jikku Jose <jikkujose@gmail.com>
2017-10-10 09:23:24 +0530

    Update Artist view

--- sites/all/themes/artchimp/views/views-view--list-artists--page-1.tpl.php
+++ sites/all/themes/artchimp/views/views-view--list-artists--page-1.tpl.php
@@ -1,18 +1,13 @@
 <?php if ($rows) : ?>
-<?php foreach ($view
-    ->style_plugin->rendered_fields as $key => $value) {
-?>
-<div class="member col-md-3 col-sm-3">
-    <div class="member-image">
-        <?php echo $value['field_photo'] ?>
-    </div>
-    <?php
-    $firstname = $value['title'] . ' ' . $value['field_lastname'];
-?>
-    <h3><?php echo $firstname; ?></h3>
-    <span>Artist</span>
-</div>
-<?php
-} ?>
-<?php
-endif; ?>
+
+<?php foreach ($view ->style_plugin->rendered_fields as $key => $value) { ?>
+  <div class="col-md-3 col-sm-3">
+    <div class=""> <?php echo $value['field_photo'] ?> </div>
+
+    <?php $full_name = $value['title'] . ' ' . $value['field_lastname']; ?>
+
+    <h2 class="mt3 b"><?php echo $full_name; ?></h2>
+  </div>
+<?php } ?>
+
+<?php endif; ?>
