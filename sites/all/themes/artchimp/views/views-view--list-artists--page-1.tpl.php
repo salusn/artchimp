5a92465b3d54ea3f85beefa53783985a6cfdeb8b
Jikku Jose <jikkujose@gmail.com>
2017-10-10 11:29:47 +0530

    Formatting..

--- sites/all/themes/artchimp/views/views-view--list-artists--page-1.tpl.php
+++ sites/all/themes/artchimp/views/views-view--list-artists--page-1.tpl.php
@@ -1,17 +1,9 @@
 <?php if ($rows) : ?>
-<?php foreach ($view
-    ->style_plugin->rendered_fields as $key => $value) {
-?>
-<div class="member col-md-4 col-sm-3">
-    <div class="member-image">
-        <?php echo $value['field_photo'] ?>
+  <?php foreach ($view ->style_plugin->rendered_fields as $key => $value) { ?>
+    <div class="member col-md-4 col-sm-3">
+      <div class="member-image"><?php echo $value['field_photo'] ?></div>
+      <?php $firstname = $value['title'] . ' ' . $value['field_lastname']; ?>
+      <div class="f2 b mt4"><?php echo $firstname; ?></div>
     </div>
-    <?php
-    $firstname = $value['title'] . ' ' . $value['field_lastname'];
-?>
-    <div class="f2 b mt4"><?php echo $firstname; ?></div>
-</div>
-<?php
-} ?>
-<?php
-endif; ?>
+  <?php } ?>
+<?php endif; ?>
