a1033ee3d849917ee104d4c301f3d3aa67bdf5b6
Light Server <light@dev.lightrains.com>
2017-10-10 19:35:57 +0530

    Added module for image url formater

--- sites/all/themes/artchimp/views/views-view--list-blogs--page-1.tpl.php
+++ sites/all/themes/artchimp/views/views-view--list-blogs--page-1.tpl.php
@@ -1,11 +1,16 @@
 <?php if ($rows): ?>
 <?php foreach ($view->style_plugin->rendered_fields as $key => $value) {
+	//print_r($value);exit;
 	?>
-<div class="member col-md-4 col-sm-3">
+<div class="member col-md-4">
     <div class="member-image">
         <?php echo $value['field_author'] ?>
-    </div>
 
+    </div>
+    <div class="member-image">
+    <?php echo $value['field_date'] ?>
+    </div>
+    <div class="f2 b mt4"><?php echo $value['title']; ?></div>
     <div class="f2 b mt4"><?php echo $value['title']; ?></div>
 </div>
 <?php
