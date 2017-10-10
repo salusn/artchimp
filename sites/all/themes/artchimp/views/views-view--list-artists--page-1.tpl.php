97db1ff09dd0d0a32259d22b7d0a3d3b54c0d232
Jikku Jose <jikkujose@gmail.com>
2017-10-10 09:32:43 +0530

    Change size of Artist image

--- sites/all/themes/artchimp/views/views-view--list-artists--page-1.tpl.php
+++ sites/all/themes/artchimp/views/views-view--list-artists--page-1.tpl.php
@@ -2,7 +2,7 @@
 <?php foreach ($view
     ->style_plugin->rendered_fields as $key => $value) {
 ?>
-<div class="member col-md-3 col-sm-3">
+<div class="member col-md-4 col-sm-3">
     <div class="member-image">
         <?php echo $value['field_photo'] ?>
     </div>
