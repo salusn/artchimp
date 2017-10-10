dd30ad3be2ee97285b5bdf41397db414593a1ee0
Light Server <light@dev.lightrains.com>
2017-10-10 19:34:14 +0530

    Title link changes

--- sites/all/themes/artchimp/templates/node--work.tpl.php
+++ sites/all/themes/artchimp/templates/node--work.tpl.php
@@ -1,16 +1,21 @@
 <div id="node-<?php print $node->nid;?>" class="<?php print $classes;?> clearfix"<?php print $attributes;?>>
 
   <div class="content"<?php print $content_attributes;?>>
-
+     <?php global $user;?>
   <div class="blog-post">
         <div class="<?php echo ($page) ? "post-img" : "post-prev-img"; ?>">
+
          <?php print render($content['field_photo']);?>
        </div>
 
        <?php print render($title_prefix);?>
+
        <?php if (!$page): ?>
          <div class="post-prev-title mb-5">
-         <h3<?php print $title_attributes;?>><a href="<?php print $node_url;?>"><?php print $title;?></a></h3>
+         <?php if ($user->roles[4]) {?>
+          <h3<?php print $title_attributes;?>><a href="<?php echo url('node/' . $node->nid . '/edit') ?>"><?php print $title;?></a></h3>
+          <?php } else {?>
+         <h3<?php print $title_attributes;?>><a href="<?php print $node_url;?>"><?php print $title;?></a></h3><?php }?>
        </div>
        <?php endif;?>
        <?php print render($title_suffix);?>
