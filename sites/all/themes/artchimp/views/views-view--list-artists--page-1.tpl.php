21398cdee01106546d3a3cec21bc83bfb1226b69
Light Server <light@dev.lightrains.com>
2017-10-11 14:32:09 +0530

    Added row

--- sites/all/themes/artchimp/views/views-view--list-artists--page-1.tpl.php
+++ sites/all/themes/artchimp/views/views-view--list-artists--page-1.tpl.php
@@ -1,11 +1,13 @@
 <?php if ($rows): ?>
-<?php foreach ($view->style_plugin->rendered_fields as $key => $value) {
+	<div class="row">
+		<?php foreach ($view->style_plugin->rendered_fields as $key => $value) {
 	?>
-<div class="member col-md-4 col-sm-3">
-    <div class="member-image">
-        <?php echo $value['field_photo'] ?>
-    </div>
-    <div class="f2 b mt4"><?php echo $value['nothing']; ?></div>
-</div>
-<?php }?>
+		<div class="member col-md-4 col-sm-3">
+		    <div class="member-image">
+		        <?php echo $value['field_photo'] ?>
+		    </div>
+		    <div class="f2 b mt4"><?php echo $value['nothing']; ?></div>
+		</div>
+		<?php }?>
+	</div>
 <?php endif;?>
