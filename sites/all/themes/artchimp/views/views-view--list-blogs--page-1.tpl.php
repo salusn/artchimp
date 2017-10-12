e9f9ad5238b332fa2f41a1d2958fef1a9b108110
Jikku Jose <jikkujose@gmail.com>
2017-10-12 17:09:19 +0530

    Automated commit

--- sites/all/themes/artchimp/views/views-view--list-blogs--page-1.tpl.php
+++ sites/all/themes/artchimp/views/views-view--list-blogs--page-1.tpl.php
@@ -2,7 +2,7 @@
   <?php foreach ($view->style_plugin->rendered_fields as $key => $value) { ?>
     <div class="jikku-row row">
       <div class="jikku-column col-md-12">
-        <div class="ph3">
+        <div class="ph3 pl0-ns">
           <div class="jikku-title f2 b"><?php echo $value['title']; ?></div>
           <div class="jikku-meta flex">
             By
