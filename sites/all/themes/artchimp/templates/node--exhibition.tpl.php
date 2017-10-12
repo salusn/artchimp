c682f3ada0d3a56f87f50e0b40d812f4bd09791b
Jikku Jose <jikkujose@gmail.com>
2017-10-12 16:22:38 +0530

    Automated commit

--- sites/all/themes/artchimp/templates/node--exhibition.tpl.php
+++ sites/all/themes/artchimp/templates/node--exhibition.tpl.php
@@ -4,6 +4,7 @@
   $end_date = $node->field_exhibition_date['und']['0']['value2'];
 
   $date_range = formatted_date_range($start_date, $end_date)
+  print $node
 ?>
 
 <div id="node-<?php print $node->nid;?>" class="<?php print $classes;?> clearfix"<?php print $attributes;?>>
@@ -26,32 +27,29 @@
         <?php endif; ?>
 
         <?php print render($title_suffix);?>
-        <?php if ($page): ?>
 
-  <div class="row mb6">
-    <div class="col-md-8">
-      <div class="<?php echo ($page) ? "post-img" : ""; ?>">
-        <?php print render($content['field_photo']);?>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="m22t4">
-        <div class="ttu black-40 b"><?php print render($content['field_location_new']);?></div>
-        <div class=""><?php print formatted_date_range($start_date, $end_date)?></div>
-        <div class=""><?php print render($content['body'])?></div>
-          <?php if (isset($user->roles[3]) || isset($user->roles[4])) {?>
-          <a href="<?php echo url('settings/exhibition/' . $node->nid) ?>">Add Works</a>
-        <?php }?>
-      </div>
-    </div>
-  </div>
-          <hr class="mt-0 mb-0">
-           <h3>Artists</h3>
-            <?php print views_embed_view('list_exhibitions', 'block_2');?>
+        <?php if ($page): ?>
+          <div class="row mb6">
+            <div class="col-md-8">
+              <div class="<?php echo ($page) ? "post-img" : ""; ?>">
+                <?php print render($content['field_photo']);?>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="m22t4">
+                <div class="ttu black-40 b"><?php print render($content['field_location_new']);?></div>
+                <div class=""><?php print formatted_date_range($start_date, $end_date)?></div>
+                <div class=""><?php print render($content['body'])?></div>
+              </div>
+            </div>
+          </div>
             <hr class="mt-0 mb-0">
-            <h3>Selected Works</h3>
-            <?php print views_embed_view('list_works', 'block_3');?>
-    </div>
+             <h3>Artists</h3>
+              <?php print views_embed_view('list_exhibitions', 'block_2');?>
+              <hr class="mt-0 mb-0">
+              <h3>Selected Works</h3>
+              <?php print views_embed_view('list_works', 'block_3');?>
+          </div>
         <?php endif;?>
 
     </div>
