14a4e8d0e26a01c72a00a7034210069813aeaab1
Jikku Jose <jikkujose@gmail.com>
2017-10-12 09:27:32 +0530

    Update home page design

--- sites/all/themes/artchimp/views/views-view-unformatted--home-main-image--block.tpl.php
+++ sites/all/themes/artchimp/views/views-view-unformatted--home-main-image--block.tpl.php
@@ -6,9 +6,10 @@
 			<a href="<?php print $value['path']?>"><img src="<?php print $value['field_photo']?>" class="img-responsive-front" /></a>
 		</div>
 		<div class="col-md-4">
-      <div class="">
-        <div class=""><?php print $value['field_location_new']?></div>
-        <div class=""><?php print $value['title']?></div>
+      <div class="mt4">
+        <div class="ttu black-40"><?php print $value['field_location_new']?></div>
+        <div class="f2 b mv2"><?php print $value['title']?></div>
+        <div class="">April 23 — May 4, 2017</div>
         <div class=""><?php print $value['field_exhibition_date']?></div>
         <div class=""><?php print $value['body']?></div>
       </div>
