68a34a67fff506a3a225337ac11f56aefe02fe31
Jikku Jose <jikkujose@gmail.com>
2017-10-12 16:18:01 +0530

    Automated commit

--- sites/all/themes/artchimp/templates/node--exhibition.tpl.php
+++ sites/all/themes/artchimp/templates/node--exhibition.tpl.php
@@ -1,33 +1,31 @@
 <?php // Jikku | Working ?>
+<?php
+  $start_date = $node->field_exhibition_date['und']['0']['value'];
+  $end_date = $node->field_exhibition_date['und']['0']['value2'];
+
+  $date_range = formatted_date_range($start_date, $end_date)
+?>
+
 <div id="node-<?php print $node->nid;?>" class="<?php print $classes;?> clearfix"<?php print $attributes;?>>
   <div class="content"<?php print $content_attributes;?>>
     <div class="blog-post">
-
-      <div class="jikku-meta lh-solid mt3 mb5">
-
-
-
         <?php print render($title_prefix);?>
         <?php if (!$page): ?>
-
-                <div class="<?php echo ($page) ? "post-img" : ""; ?>">
-        <?php print render($content['field_photo']);?>
-      </div>
-
-          <div class="jikku-location ttu b black-60">Hardcoded Location</div>
-          <div class="jikku-title f2 b black-80 mt2 mb3">
-            <a href="<?php print $node_url;?>">
-              <?php print $title;?>
-            </a>
+          <div class="<?php echo ($page) ? "post-img" : ""; ?>">
+            <?php print render($content['field_photo']);?>
           </div>
-        <?php endif;?>
-        <?php print render($title_suffix);?>
-        <?php
-$start_date = $node->field_exhibition_date['und']['0']['value'];
-$end_date = $node->field_exhibition_date['und']['0']['value2'];
+          <div class="jikku-meta lh-solid mt3 mb5">
+            <div class="jikku-location ttu b black-60">Hardcoded Location</div>
+            <div class="jikku-title f2 b black-80 mt2 mb3">
+              <a href="<?php print $node_url; ?>">
+                <?php print $title;?>
+              </a>
+            </div>
+            <div><?php echo $date_range; ?></div>
+          </div>
+        <?php endif; ?>
 
-?>
-      </div>
+        <?php print render($title_suffix);?>
         <?php if ($page): ?>
 
   <div class="row mb6">
