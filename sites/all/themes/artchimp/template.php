af2d5f5db4e1cc4f2bac32f79a04884162ed8d52
Jikku Jose <jikkujose@gmail.com>
2017-10-12 14:32:38 +0530

    Refactor

--- sites/all/themes/artchimp/template.php
+++ sites/all/themes/artchimp/template.php
@@ -16,3 +16,10 @@ function artchimp_preprocess_page(&$variables) {
 function exhibition_date_markup($range) {
 	return "<div class='jikku-date'>$range</div>";
 }
+
+function formatted_date_range($date_1, $date_2) {
+  $start_date = date("d F", strtotime($date_1));
+  $end_date = date("d F Y", strtotime($date_2));
+
+  return "$start_date — $end_date";
+}
