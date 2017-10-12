e8850c6f9d34ee420c50c708916dedb40eb1eac9
Jikku Jose <jikkujose@gmail.com>
2017-10-12 15:34:36 +0530

    Automated commit

--- sites/all/themes/artchimp/templates/node--exhibition.tpl.php
+++ sites/all/themes/artchimp/templates/node--exhibition.tpl.php
@@ -1,15 +1,15 @@
-<?php // Jikku | Working ?>
+<?php // Jikku | Done ?>
 <div id="node-<?php print $node->nid;?>" class="<?php print $classes;?> clearfix"<?php print $attributes;?>>
   <div class="content"<?php print $content_attributes;?>>
     <div class="blog-post">
       <div class="<?php echo ($page) ? "post-img" : ""; ?>">
         <?php print render($content['field_photo']);?>
       </div>
-      <div class="jikku-meta lh-solid mt3 mb5">
+      <div class="lh-solid mt3 mb5">
         <?php print render($title_prefix);?>
         <?php if (!$page): ?>
-          <div class="jikku-location ttu b black-60">Hardcoded Location</div>
-          <div class="jikku-title f2 b black-80 mt2 mb3">
+          <div class="ttu b black-60">Hardcoded Location</div>
+          <div class="f2 b black-80 mt2 mb3">
             <a href="<?php print $node_url;?>">
               <?php print $title;?>
             </a>
@@ -20,11 +20,11 @@
           $start_date = $node->field_exhibition_date['und']['0']['value'];
           $end_date = $node->field_exhibition_date['und']['0']['value2'];
 
-          echo "<div class='jikku-date'>" . formatted_date_range($start_date, $end_date) . "</div>";
+          echo "<div>" . formatted_date_range($start_date, $end_date) . "</div>";
         ?>
       </div>
         <?php if ($page): ?>
-          <div class="jikku-delete">
+          <div>
             <?php
               hide($content['comments']);
               hide($content['links']);
