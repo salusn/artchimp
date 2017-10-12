751b85182fb34330deae03e347248ecf0e00ea91
Jikku Jose <jikkujose@gmail.com>
2017-10-12 16:46:55 +0530

    Automated commit

--- sites/all/themes/artchimp/templates/node--exhibition.tpl.php
+++ sites/all/themes/artchimp/templates/node--exhibition.tpl.php
@@ -3,13 +3,11 @@
   $start_date = $node->field_exhibition_date['und']['0']['value'];
   $end_date = $node->field_exhibition_date['und']['0']['value2'];
 
-  $date_range = formatted_date_range($start_date, $end_date)
+  $date_range = formatted_date_range($start_date, $end_date);
+  $location = $node->field_location_new['und']['0']['value'];
 ?>
 
 <div id="node-<?php print $node->nid;?>" class="<?php print $classes;?> clearfix"<?php print $attributes;?>>
-  <code>
-    <?php echo $node->field_location_new['und']['0']['value']; ?>
-  </code>
   <div class="content"<?php print $content_attributes;?>>
     <div class="blog-post">
         <?php print render($title_prefix);?>
@@ -18,7 +16,7 @@
             <?php print render($content['field_photo']);?>
           </div>
           <div class="jikku-meta lh-solid mt3 mb5">
-            <div class="jikku-location ttu b black-60">Hardcoded Location</div>
+          <div class="jikku-location ttu b black-60"><?= $location ?></div>
             <div class="jikku-title f2 b black-80 mt2 mb3">
               <a href="<?php print $node_url; ?>">
                 <?php print $title;?>
