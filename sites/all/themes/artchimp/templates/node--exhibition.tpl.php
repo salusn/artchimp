3f3b8281a1b2a189b0d638a0468fd2720e288afc
Jikku Jose <jikkujose@gmail.com>
2017-10-12 16:31:07 +0530

    Automated commit

--- sites/all/themes/artchimp/templates/node--exhibition.tpl.php
+++ sites/all/themes/artchimp/templates/node--exhibition.tpl.php
@@ -8,7 +8,7 @@
 
 <div id="node-<?php print $node->nid;?>" class="<?php print $classes;?> clearfix"<?php print $attributes;?>>
   <code>
-    <?php echo json_encode(get_object_vars($node->field_location_new)); ?>
+    <?php echo json_encode(get_object_vars($node->field_location_new())); ?>
   </code>
   <div class="content"<?php print $content_attributes;?>>
     <div class="blog-post">
