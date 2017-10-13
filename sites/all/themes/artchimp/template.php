8491e2058e8406465e4cb01cafbcfaa9ec1dbb87
Light Server <light@dev.lightrains.com>
2017-10-13 16:37:38 +0530

    Changed name section

--- sites/all/themes/artchimp/template.php
+++ sites/all/themes/artchimp/template.php
@@ -2,15 +2,6 @@
 
 function artchimp_preprocess_page(&$variables) {
 
-	if (arg(0) == 'node') {
-
-		if (isset($variables['node']->field_lastname['und']['0']['value'])) {
-
-			$page_title = $variables['node']->title . ' ' . $variables['node']->field_lastname['und']['0']['value'];
-
-			$variables['title'] = $page_title;
-		}
-	}
 }
 
 function exhibition_date_markup($range) {
--- sites/all/themes/artchimp/views/views-view--list-artists--page-1.tpl.php
+++ sites/all/themes/artchimp/views/views-view--list-artists--page-1.tpl.php
@@ -1,14 +1,14 @@
 <?php if ($rows): ?>
 	<div class="row">
     <?php foreach ($view->style_plugin->rendered_fields as $key => $value) {
-      $photo = $value['field_photo'];
-      $name = $value['nothing'];
-    ?>
+	$photo = $value['field_photo'];
+	$name = $value['title'];
+	?>
       <div class="col-md-4 col-sm-3 mb5">
           <div class="member-image">
-              <?= $photo ?>
+              <?=$photo?>
           </div>
-          <div class="f2 b mv3"><?= $name ?></div>
+          <div class="f2 b mv3"><?=$name?></div>
       </div>
 		<?php }?>
 	</div>
