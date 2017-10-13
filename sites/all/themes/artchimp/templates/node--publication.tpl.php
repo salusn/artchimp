fab2f20baaf0c45e80b4164c1264e2822c4e6a64
Light Server <light@dev.lightrains.com>
2017-10-13 12:56:23 +0530

    Added tpl for publications detail page

---
+++ sites/all/themes/artchimp/templates/node--publication.tpl.php
@@ -0,0 +1,21 @@
+<?php $isDetailPage = $page;?>
+<div id="node-<?php print $node->nid;?>" class="<?php print $classes;?> clearfix"<?php print $attributes;?>>
+  <div class="content"<?php print $content_attributes;?>>
+    <div class="blog-post">
+        <?=render($title_prefix)?>
+        <?php if ($isDetailPage): ?>
+          <div class="row mb6">
+            <div class="col-md-8">
+              <div class="<?php echo ($isDetailPage) ? "post-img" : ""; ?>">
+                <?php print render($content['field_photo']);?>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class=""><?=render($content['body'])?></div>
+            </div>
+          </div>
+          </div>
+        <?php endif;?>
+    </div>
+  </div>
+</div>
