945f7cf44ce34b956ce586b8cf4e9a303dd2b11b
Light Server <light@dev.lightrains.com>
2017-10-13 12:58:13 +0530

    Added comment

--- sites/all/themes/artchimp/templates/node--publication.tpl.php
+++ sites/all/themes/artchimp/templates/node--publication.tpl.php
@@ -1,3 +1,4 @@
+<?php //Jikku publication details page?>
 <?php $isDetailPage = $page;?>
 <div id="node-<?php print $node->nid;?>" class="<?php print $classes;?> clearfix"<?php print $attributes;?>>
   <div class="content"<?php print $content_attributes;?>>
