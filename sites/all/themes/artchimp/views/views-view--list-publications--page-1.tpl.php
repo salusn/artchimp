f39012e8bb107ec92cf946b951abcce42b11665f
Jikku Jose <jikkujose@gmail.com>
2017-10-13 15:39:02 +0530

    Automated commit

--- sites/all/themes/artchimp/views/views-view--list-publications--page-1.tpl.php
+++ sites/all/themes/artchimp/views/views-view--list-publications--page-1.tpl.php
@@ -1,16 +1,14 @@
-<?php // Jikku publication listing?>
+<?php // Jikku | Working ?>
 <?php if ($rows): ?>
-	<div class="row">
+  <div class="row">
     <?php foreach ($view->style_plugin->rendered_fields as $key => $value) {
-	$photo = $value['field_photo'];
-	$name = $value['title'];
-	?>
+      $photo = $value['field_photo'];
+      $name = $value['title'];
+    ?>
       <div class="col-md-4 col-sm-3 mb5">
-          <div class="member-image">
-              <?=$photo?>
-          </div>
-          <div class="f2 b mv3"><?=$name?></div>
+        <div class="member-image"><?= $photo ?></div>
+        <div class="f2 b mv3"><?= $name ?></div>
       </div>
-		<?php }?>
-	</div>
-<?php endif;?>
+    <?php } ?>
+  </div>
+<?php endif ?>
